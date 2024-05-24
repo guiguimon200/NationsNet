01bce56d234b3fff1fef9c549fbe23e7df57c102
guiguimon200 <guilherme.brito877@sptech.school>
2024-05-24 17:23:06 -0300

    Atualizações na Modelagem e Script do Banco de Dados, e alterações no codigo

---
+++ Banco_de_Dados/script_NationsNet.sql
@@ -0,0 +1,41 @@
+drop database nationsnet;
+create database nationsnet;
+
+use nationsnet;
+
+create table usuario (
+	idUsuario int primary key AUTO_INCREMENT,
+	nome varchar(50),
+	ddd char(2),/*substr(0,2) */
+	prefixo char(5), /*substr(2,5) */
+	sufixo char(4), /*substr(6,4) */
+	email varchar(50), 
+	senha varchar(20)
+);
+
+create table quiz (
+	idQuiz int primary key auto_increment,
+	nome varchar(50),
+    descricao varchar(50)
+);
+
+create table tentativa (
+	fkUsuario int,
+	fkQuiz int,
+	idTentativa int,
+	dataTentativa datetime,
+	pontuacao int,
+
+	primary key (fkUsuario, fkQuiz, idTentativa),
+	foreign key (fkUsuario) references usuario (idUsuario),
+	foreign key (fkQuiz) references quiz (idQuiz)
+);
+
+create table pergunta (
+	idPergunta int primary key auto_increment,
+	pergunta varchar(200),
+	resposta varchar(45),
+	fkPerguntaQz int,
+
+	foreign key (fkPerguntaQz) references quiz (idQuiz)
+);
--- Site_Web/src/database/script-tabelas.sql
+++ Site_Web/src/database/script-tabelas.sql
@@ -6,54 +6,94 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+-- CREATE DATABASE aquatech;
 
-USE aquatech;
+-- USE aquatech;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
+-- CREATE TABLE empresa (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	razao_social VARCHAR(50),
+-- 	cnpj CHAR(14)
+-- );
+
+-- CREATE TABLE usuario (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	nome VARCHAR(50),
+-- 	email VARCHAR(50),
+-- 	senha VARCHAR(50),
+-- 	fk_empresa INT,
+-- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+-- );
+
+-- CREATE TABLE aviso (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	titulo VARCHAR(100),
+-- 	descricao VARCHAR(150),
+-- 	fk_usuario INT,
+-- 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+-- );
+
+-- create table aquario (
+-- /* em nossa regra de negócio, um aquario tem apenas um sensor */
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	descricao VARCHAR(300),
+-- 	fk_empresa INT,
+-- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+-- );
+
+-- /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+-- create table medida (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	dht11_umidade DECIMAL,
+-- 	dht11_temperatura DECIMAL,
+-- 	luminosidade DECIMAL,
+-- 	lm35_temperatura DECIMAL,
+-- 	chave TINYINT,
+-- 	momento DATETIME,
+-- 	fk_aquario INT,
+-- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+-- );
+
+-- insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
+-- insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+
+create database nationsnet;
+
+use nationsnet;
+
+create table usuario (
+	idUsuario int primary key AUTO_INCREMENT,
+	nome varchar(50),
+	ddd char(2),/*substr(0,2) */
+	prefixo char(5), /*substr(2,5) */
+	sufixo char(4), /*substr(6,4) */
+	email varchar(50), 
+	senha varchar(20)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table quiz (
+	idQuiz int primary key auto_increment,
+	nome varchar(50)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table pontuacao (
+	fkUsuario int,
+	fkQuiz int,
+	idPontuacao int primary key,
+	dtPontuacao datetime,
+	pontuacao int,
+
+	primary key (fkUsuario, fkQuiz, idPontuacao),
+	foreign key (fkUsuario) references usuario (idUsuario),
+	foreign key (fkQuiz) references quiz (idQuiz)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table resposta (
+	idQuiz int primary key auto_increment,
+	pergunta varchar(200),
+	resposta char(1),
+	fkQuizRes int,
+
+	foreign key (fkQuizRes) references quiz (idQuiz)
 );
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
